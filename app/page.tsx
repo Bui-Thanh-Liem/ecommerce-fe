700bf1cc8cab45f815566a5cd40cc724812fdb00
LiemDev <buithanhliem5073@gmail.com>
2025-12-05 23:36:31 +0700

    update code

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,16 @@
 import { useState } from "react";
 
 export default function Home() {
-  const [files, setFiles] = useState<File[]>([]);
+  const [file, setFile] = useState<File | null>(null);
 
   const handleSubmit = async (e: React.FormEvent) => {
     //
     e.preventDefault();
 
-    //
-    const file = files[0];
     if (!file) return;
 
     // Chunk size
-    const chunkSize = file.size / 20; // 3MB
+    const chunkSize = 1 * 1024 * 1024; // 1MB
     const chunks = [];
     let startPos = 0;
 
@@ -27,17 +25,29 @@ export default function Home() {
     //
     if (chunks.length === 0) return;
 
-    //
+    // const rname = Math.random().toString(36).substring(2, 6);
+    const chunkUploadPromises: any[] = [];
     chunks.map((chunk, index) => {
       const formData = new FormData();
       formData.set("name", file.name + "-" + index);
       formData.append("large-file", chunk);
 
-      fetch("http://localhost:9000/api/upload/large-file", {
-        method: "POST",
-        body: formData,
-      });
+      chunkUploadPromises.push(
+        fetch("http://localhost:9000/api/upload/large-file", {
+          method: "POST",
+          body: formData,
+        })
+      );
     });
+
+    // Merge file
+    await Promise.all(chunkUploadPromises);
+    await fetch(
+      `http://localhost:9000/api/upload/merge-file?file=${file.name}`,
+      {
+        method: "GET",
+      }
+    );
   };
 
   return (
@@ -51,9 +61,8 @@ export default function Home() {
         <input
           type="file"
           name="large-file"
-          accept="image/*"
-          multiple
-          onChange={(e) => setFiles(Array.from(e.target.files || []))}
+          accept="*"
+          onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)}
           className="border p-2 rounded"
         />
 
